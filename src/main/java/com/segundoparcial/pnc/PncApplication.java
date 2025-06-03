d9e25595a9c2df37bb8c9dc450b4d1ca79b95de5
David <00093217@uca.edu.sv>
2025-06-02 18:57:43 -0600

    Innitial Commit

---
+++ src/main/java/com/segundoparcial/pnc/PncApplication.java
@@ -0,0 +1,13 @@
+package com.segundoparcial.pnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PncApplication.class, args);
+	}
+
+}
